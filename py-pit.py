8d0f1e1846c08eff11fc61d3482a6d03b2177c69
Stef Siekman <s.siekman@gmail.com>
2019-04-16 20:38:22 +0200

    Use queue for processing ref changes

--- py-pit.py
+++ py-pit.py
@@ -1,10 +1,17 @@
 import beacon
 import refs
 import threading
+import queue
+
+
+ref_queue = queue.Queue()
 
 
 def handle_ref_change(ref, old_val, new_val):
-    print(f"{ref}\t{old_val} -> {new_val}")
+    global ref_queue
+
+    # Add the ref change to list to process
+    ref_queue.put((ref, old_val, new_val))
 
 
 if __name__ == "__main__":
@@ -17,10 +24,10 @@ if __name__ == "__main__":
     ref_thread = threading.Thread(target=refs.listen)
     ref_thread.start()
 
-    input()
-    print("Stopping...")
+    # Process incoming ref changes
+    while True:
+        ref, vold, vnew = ref_queue.get()
+        print(f"Ref change {ref}:\t{vold} -> {vnew}")
 
-    refs.stop_connection()
-    ref_thread.join()
 
 
